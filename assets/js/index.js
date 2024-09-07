20afea2ca3d809b5f306d003b4a63974f07016cd
Darwin Zamarca G <dzamarca@subsole.com>
2024-09-07 00:00:50 -0400

    add- carga inicial

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+const numero = 50000  
+const precio = numero.toLocaleString();
+let cantidad = 0
+
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio
+
+totalAPagar = document.querySelector(".valor-total")
+totalAPagar.innerHTML=0
